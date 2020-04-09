8543dc7c92e592ce3ece159cccbcada71269b23c
Alex Reed <alexreed1192@gmail.com>
2020-04-08 22:20:48 -0700

    Add Market Hours

--- tests/trading_robot.py
+++ tests/trading_robot.py
@@ -1,7 +1,5 @@
 
-import time as t
-
-from datetime import datetime, time, timezone
+import pprint
 from pyrobot.robot import PyRobot
 from tests.config import CONSUMER_ID, REDIRECT_URI, JSON_PATH, ACCOUNT_TRADING
 
@@ -11,8 +9,7 @@ trading_robot = PyRobot(consumer_id = CONSUMER_ID, redirect_uri = REDIRECT_URI, 
 # Create a Portfolio
 trading_robot_portfolio = trading_robot.create_portfolio()
 
-postions = [('MSFT', 10), ('AAPL',20), ('AA',10)]
-
+# Add a single position
 trading_robot_portfolio.add_position(
     symbol = 'MSFT', 
     quantity = 10, 
@@ -21,6 +18,7 @@ trading_robot_portfolio.add_position(
     purchase_date = '2020-04-01'
 )
 
+# Add another single position
 trading_robot_portfolio.add_position(
     symbol = 'AAPL', 
     quantity = 10, 
@@ -29,10 +27,17 @@ trading_robot_portfolio.add_position(
     purchase_date = '2020-04-01'
 )
 
-print(trading_robot_portfolio.positions)
+# Print the Positions
+pprint.pprint(trading_robot_portfolio.positions)
 
-print(trading_robot.grab_current_quotes())
+# If the Market is open, print some quotes.
+if trading_robot.regular_market_open:
+    pprint.pprint(trading_robot.grab_current_quotes())
 
-# print(trading_robot.session)
-# print(trading_robot.pre_market_open)
-# print(trading_robot.post_market_open)
+# If the Post Market is Open, do something.
+elif trading_robot.post_market_open:
+    pprint.pprint(trading_robot.grab_current_quotes())
+
+# If the Pre Market is Open, do something.
+elif trading_robot.pre_market_open:
+    pprint.pprint(trading_robot.grab_current_quotes())
