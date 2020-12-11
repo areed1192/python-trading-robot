abe7ac9189873f376e6fd968f57f403a8ffd45d2
Alex Reed <alexreed1192@gmail.com>
2020-12-11 11:34:22 -0800

    more config

--- config/write_config.py
+++ config/write_config.py
@@ -5,7 +5,7 @@ config = ConfigParser()
 
 config.add_section('main')
 config.set('main', 'CLIENT_ID', 'FAKE_CLIENT_ID')
-config.set('main', 'REDIRECT_URI', 'FAKE_URL')
+config.set('main', 'REDIRECT_URI', 'FAKE_REDIRECT_URL')
 config.set('main', 'JSON_PATH', 'FAKE_PATH')
 config.set('main', 'ACCOUNT_NUMBER', 'FAKE_ACCT')
 
