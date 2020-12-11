70d6a1403d45aae8e33f293ad61600b9ca9fec2b
Alex Reed <alexreed1192@gmail.com>
2020-12-11 10:47:23 -0800

    Add config temp

---
+++ config/write_config.py
@@ -0,0 +1,13 @@
+# python 3.x
+from configparser import ConfigParser
+
+config = ConfigParser()
+
+config.add_section('main')
+config.set('main', 'CLIENT_ID', 'FAKE_CLIENT_ID')
+config.set('main', 'REDIRECT_URI', 'FAKE_URL')
+config.set('main', 'JSON_PATH', 'FAKE_PATH')
+config.set('main', 'ACCOUNT_NUMBER', 'FAKE_ACCT')
+
+with open(file='config/config.ini', mode='w') as f:
+    config.write(f)
